7e98f48ebc08cb388fa3cb2877ad3b4d1e6456e3
wiktor8k <54490586+wiktor8k@users.noreply.github.com>
2022-04-13 20:57:18 +0200

    Add files via upload

---
+++ dc_message_sender.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+from datetime import timedelta, datetime
+
+import os
+
+intents = discord.Intents.all()
+
+
+client = commands.Bot(intents = intents, fetch_offline_members = True, command_prefix = "")
+
+guild = None
+
+fabyn = None
+fetardes = None
+
+FABYN_ID = 334766583790567425
+FETARDES_ID = 334766583790567425
+
+@client.event
+async def on_ready():
+  global guild, fabyn, fetardes
+  guild = client.guilds[0]
+
+  for member in guild.members:
+    if member.id == FABYN_ID:
+      fabyn = member
+    if member.id == FETARDES_ID:
+      fetardes = member
+
+    if fetardes != None and fabyn != None:
+      break
+
+  while True:
+    now = datetime.now() + timedelta(hours = 2)
+
+    if now.day == 14 or now.minute == 21:
+      await fetardes.send("https://youtu.be/t0k9SgCSWzQ")
+      await fetardes.send("**TO TWOJE URODZINY DZIIIIŚ**")
+      await fetardes.send("**ADAM**")
+      await fetardes.send("**...**")
+      await fetardes.send("**JA PRAWDOPODOBNIE TERA ŚPIE**")
+      await fetardes.send("**...**")
+      await fetardes.send("**ADAM**")
+      break
+    
+
+@client.event
+async def on_message(ctx):
+  if ctx.author.bot:
+    return
+
+  global fabyn
+  
+  await ctx.channel.send("twoja mama bip bop")
+
+  if fabyn != None:
+    await fabyn.send(ctx.author.name + ": " + ctx.content)
+    
+client.run("OTYzODUxMTcxMjA5MDE5Mzky.YlcGWg.isHiOPYaRnGU1Ej2m0R1qbkPkfw")
