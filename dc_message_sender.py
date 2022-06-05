85f7444f2bb4a38ac85cfce4cb0679900f50eb84
wiktor8k <54490586+wiktor8k@users.noreply.github.com>
2022-06-05 23:05:13 +0200

    Update dc_message_sender.py

--- dc_message_sender.py
+++ dc_message_sender.py
@@ -1,4 +1,3 @@
-https://www.youtube.com/watch?v=hY0EBSpBK-s&ab_channel=BritishAgent
 
 import discord, asyncio
 from discord.ext import commands
@@ -8,32 +7,21 @@ import os
 
 intents = discord.Intents.all()
 
+FABYN_ID = 334766583790567425
 
 client = commands.Bot(intents = intents, fetch_offline_members = True, command_prefix = "")
 
 guild = None
 
-fabyn = None
-fetardes = None
-
-FABYN_ID = 334766583790567425
-FETARDES_ID = 315048337612996608
-
 @client.event
 async def on_ready():
-  global guild, fabyn, fetardes
+  global guild
   guild = client.guilds[0]
 
   for member in guild.members:
-    if member.id == FABYN_ID:
-      fabyn = member
-    if member.id == FETARDES_ID:
-      fetardes = member
+   if member.id == FABYN_ID:
+     fabyn = member
 
-    if fetardes != None and fabyn != None:
-      break
-      
-      
   await fabyn.send("Ready! Steady!")
   print("Ready! Steady!")
 
@@ -41,29 +29,30 @@ async def on_ready():
     await asyncio.sleep(1)
     now = datetime.now() + timedelta(hours = 2)
     
-    if now.day == 4:
-      await fabyn.send("https://youtu.be/t0k9SgCSWzQ", reference = 966293238388490270)
-      break
     
 
 @client.event
 async def on_message(ctx):
+
+# retrieve avatar command
   if "avatar" in ctx.content and client.id in [member.id for member in ctx.mentions]:
     for member in ctx.mentions:
       if member.id != client.id:
         await ctx.channel.send(member.avatar_url)
         
-  
-  if ctx.author.bot or not isinstance(ctx.channel, discord.channel.DMChannel):
-    return
-
-  global fabyn
-  
-  await ctx.channel.send("twoja mama bip bop")
-
-  if fabyn != None:
-    await fabyn.send(ctx.author.name + ": " + ctx.content)
-    
+# other utilities
+  if ctx.channel.id == 983107236693954561:
+    if len(ctx.attachments) != 0:
+      await ctx.delete()
+      
+    else:
+      counter = 0
+      async for message in ctx.channel.history(limit=200):
+        if message.author == ctx.author:
+          counter += 1
+        if counter == 2:
+          await ctx.delete()
+          break
     
 token = "OTYzODUxMTcxMjA5MDE5Mzky." + "YlcGWg.J-ylJLO" + "YF7NphOfUZHiMZCpAbaQ"
 client.run(token)
